0b038df42291deb45f5bb76d7d569edae81b18dc
HughTrann <tranq18@student.douglascollege.ca>
2024-07-31 10:38:58 -0700

    4th commit

--- script/script-contact.js
+++ script/script-contact.js
@@ -1,5 +1,5 @@
 function sendClick(){
     alert("Your message have been sent to us. We will contact you at our soonest. \n\n Have a nice day !!!")
     event.preventDefault();
-    window.location.href = "home.html";
+    window.location.href = "index.html";
 }
